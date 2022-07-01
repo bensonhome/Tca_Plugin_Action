2556d7387441295f705df4c448a0771da32b2cd4
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-01 11:00:23 +0800

    1234

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-// const core = require('@actions/core')
-// const child_process = require('child_process')
-// const label = core.getInput('label')
-// const client_path = __dirname + '/../lib/tca-client-linux'
+const core = require('@actions/core')
+const child_process = require('child_process')
+const label = core.getInput('label')
+const client_path = __dirname + '/../lib/tca-client-linux'
 
-// try{
-//     const cmd_init = './codepuppy quickinit --label ' + label
-//     child_process.execSync(cmd_init, { client_path }, function(error, stdout, stderr){
-//         if (error){
-//             core.error(stderr)
-//             return
-//         }
-//         core.info(stderr)
-//     })
-// } catch (error){
-//     core.error(error.message);
-// }
+try{
+    const cmd_init = './codepuppy quickinit --label ' + label
+    child_process.execSync(cmd_init, { client_path }, function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        core.info(stderr)
+    })
+} catch (error){
+    core.error(error.message);
+}
 
 // try {
 //     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
@@ -28,21 +28,3 @@
 // } catch (error) {
 //     core.error(error.message)
 // }
-
-const core = require('@actions/core')
-const child_process = require('child_process')
-
-const label = core.getInput('label')
-const cwd = __dirname + '/../lib/tca-client-linux'
-try{
-    const cmd_init = './codepuppy quickinit --label ' + label
-    child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
-        if (error){
-            core.error(stderr)
-            return
-        }
-        core.info(stderr)
-    })
-} catch (error){
-    core.error(error.message);
-}
