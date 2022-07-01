85fb84cf7bf367eb7bd07e6c9a6e81d9ff389c39
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-01 10:57:59 +0800

    1234

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+// const core = require('@actions/core')
+// const child_process = require('child_process')
+// const label = core.getInput('label')
+// const client_path = __dirname + '/../lib/tca-client-linux'
+
+// try{
+//     const cmd_init = './codepuppy quickinit --label ' + label
+//     child_process.execSync(cmd_init, { client_path }, function(error, stdout, stderr){
+//         if (error){
+//             core.error(stderr)
+//             return
+//         }
+//         core.info(stderr)
+//     })
+// } catch (error){
+//     core.error(error.message);
+// }
+
+// try {
+//     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
+//     child_process.execSync(cmd_scan, { client_path }, function(error, stdout, stderr){
+//         if (error){
+//             core.error(stderr)
+//             return
+//         }
+//         core.info(stderr)
+//     })
+// } catch (error) {
+//     core.error(error.message)
+// }
+
 const core = require('@actions/core')
 const child_process = require('child_process')
-const label = core.getInput('label')
-const client_path = __dirname + '/../lib/tca-client-linux'
 
+const label = core.getInput('label')
+const cwd = __dirname + '/../lib/tca-client-linux'
 try{
     const cmd_init = './codepuppy quickinit --label ' + label
-    child_process.execSync(cmd_init, { client_path }, function(error, stdout, stderr){
+    child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
             return
@@ -14,17 +45,4 @@ try{
     })
 } catch (error){
     core.error(error.message);
-}
-
-try {
-    const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
-    child_process.execSync(cmd_scan, { client_path }, function(error, stdout, stderr){
-        if (error){
-            core.error(stderr)
-            return
-        }
-        core.info(stderr)
-    })
-} catch (error) {
-    core.error(error.message)
 }
