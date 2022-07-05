25311377a35df1028c329af4f228e20edd56e7e1
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-05 09:43:43 +0800

    11

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 const core = require('@actions/core')
 const child_process = require('child_process')
 const label = core.getInput('label')
-const c = __dirname + '/../lib/tca-client-linux'
+const cwd = __dirname + '/../lib/tca-client-linux'
 
 try{
     const cmd_init = './codepuppy quickinit --label ' + label
-    child_process.execSync(cmd_init, { c }, function(error, stdout, stderr){
+    child_process.execSync(cmd_init, { cwd }, function(error, stdout, stderr){
         if (error){
             core.error(stderr)
             return
         }
-        core.info(stderr)
+        // core.info(stderr)
     })
 } catch (error){
     core.error(error.message);
 }
 
-// try {
-//     const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
-//     child_process.execSync(cmd_scan, { client_path }, function(error, stdout, stderr){
-//         if (error){
-//             core.error(stderr)
-//             return
-//         }
-//         core.info(stderr)
-//     })
-// } catch (error) {
-//     core.error(error.message)
-// }
+try {
+    const cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
+    child_process.execSync(cmd_scan, { client_path }, function(error, stdout, stderr){
+        if (error){
+            core.error(stderr)
+            return
+        }
+        // core.info(stderr)
+    })
+} catch (error) {
+    core.error(error.message)
+}
