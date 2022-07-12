6a43d257c55daea07b61e8cb0d9c357d868cfaaf
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-12 16:26:44 +0800

    123

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ if(os=='linux'){
     var cmd_init = 'codepuppy.exe quickinit --label ' + label
     var cmd_scan = 'codepuppy.exe quickscan --label ' + label + ' -s ' + process.cwd()
     var cwd = __dirname + '/../lib/tca-client-windows'
-}else if(os=='mac'){
+}else if(os=='darwin'){
     var cmd_init = './codepuppy quickinit --label ' + label
     var cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
     var cwd = __dirname + '/../lib/tca-client-mac'
