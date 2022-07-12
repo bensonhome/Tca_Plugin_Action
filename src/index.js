6bee0db103c69e0e8fbc2426a3ced77cfa728f49
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-12 12:38:04 +0800

    操作系统

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const core = require('@actions/core')
 const child_process = require('child_process')
 const label = core.getInput('label')
 const cwd = __dirname + '/../lib/tca-client-linux'
-const fs = require('fs')
+
+try {
+    const os = process.platform
+    core.info(os)
+} catch (error) {
+    core.error(error.message)
+}
+
 
 try{
     const cmd_init = './codepuppy quickinit --label ' + label
@@ -29,7 +36,8 @@ try {
 }
 
 try{
-   fs.readFile(cwd + '/tca_quick_scan_report.json', (err, buffer) => {
+    const fs = require('fs')
+    fs.readFile(cwd + '/tca_quick_scan_report.json', (err, buffer) => {
     if(err) console.error(err)
     let d = buffer.toString()
     core.info(d)
