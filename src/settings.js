40e36c8f533e54424d2a6168fe91791d389291d3
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-13 11:15:55 +0800

    234

--- src/settings.js
+++ src/settings.js
@@ -1,12 +1,12 @@
-export const dir = __dirname + '/../lib'
+exports.dir = __dirname + '/../lib'
 
-export const linuxURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
-export const linuxName = linuxURL.split('/')[linuxURL.split('/').length - 1]
+exports.linuxURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+exports.linuxName = linuxURL.split('/')[linuxURL.split('/').length - 1]
 
-export const darwinURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-apple-darwin.zip'
-export const darwinName = darwinURL.split('/')[darwinURL.split('/').length - 1]
+exports.darwinURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-apple-darwin.zip'
+exports.darwinName = darwinURL.split('/')[darwinURL.split('/').length - 1]
 
-export const windowsURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-windows.zip'
-export const windowsName = windowsURL.split('/')[windowsURL.split('/').length - 1]
+exports.windowsURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-windows.zip'
+exports.windowsName = windowsURL.split('/')[windowsURL.split('/').length - 1]
 
 
