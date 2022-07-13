7b6b0aeb080944f51d547ed69e2f4f5c523072d5
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-13 11:23:07 +0800

    qwe

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,7 @@
 exports.dir = __dirname + '/../lib'
 
-exports.linuxURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+var linuxURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+exports.linuxURL = linuxURL
 exports.linuxName = linuxURL.split('/')[linuxURL.split('/').length - 1]
 
 exports.darwinURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-apple-darwin.zip'
@@ -8,5 +9,3 @@ exports.darwinName = darwinURL.split('/')[darwinURL.split('/').length - 1]
 
 exports.windowsURL = 'https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-windows.zip'
 exports.windowsName = windowsURL.split('/')[windowsURL.split('/').length - 1]
-
-
