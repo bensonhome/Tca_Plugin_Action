8ee5c967b8aaa772c94f481abafbd5a9b8fd146a
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-13 17:35:21 +0800

    v3

--- src/compressFile.js
+++ src/compressFile.js
@@ -5,8 +5,12 @@ function compressFile(filepath, filedir){
     const compressing = require('compressing')
 
     try {
-        compressing.gzip.uncompress(filepath, filedir).then(() => {
+        compressing.gzip.uncompress(filepath, filedir)
+        .then(() => {
             core.info('解压成功')
+        })
+        .catch((err) => {
+            core.error(err)
         })
     } catch (error) {
         core.info(error)
