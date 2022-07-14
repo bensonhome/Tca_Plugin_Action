56cf9b078a9b3d0aea3becb5cf1d77c6e8af1712
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-14 16:07:50 +0800

    qqqqq

--- src/compressFile.js
+++ src/compressFile.js
@@ -4,11 +4,11 @@ function compressFile(filepath, filedir){
     const core = require('@actions/core')
     const compressing = require('compressing')
 
-        compressing.zip.uncompress(filepath, filedir)
-        .then(() => {
-            core.info('解压成功')
-        })
-        .catch((err) => {
-            core.error(err)
-        })
+    compressing.zip.uncompress(filepath, filedir)
+    .then(() => {
+        core.info('解压成功')
+    })
+    .catch((err) => {
+        core.error(err)
+    })
 }
--- src/downloadFile.js
+++ src/downloadFile.js
@@ -3,11 +3,31 @@ exports.downloadFile = downloadFile
 function downloadFile(url, fileName, dir){
     const core = require('@actions/core')
     const fs = require('fs')
-    const path = require('path')
-    const request = require('sync-request')
+    // const path = require('path')
+    // const request = require('request')
 
-    var stream = fs.createWriteStream(path.join(dir, fileName))
-    request(url).pipe(stream).on('close', function(err){
-        core.info('文件【' + fileName + '】下载完毕')
-    })
+    // var stream = fs.createWriteStream(path.join(dir, fileName))
+    // request(url).pipe(stream).on('close', function(err){
+    //     core.info('文件【' + fileName + '】下载完毕')
+    // })
+
+    var express = require('express')
+    var router = express.Router()
+    var unzip = require('unzip')
+    const download = require('download')
+
+    (async () => {
+        fs.writeFileSync(dir + '/' + fileName, await download(url))
+
+        var extract = unzip.Extract({ path: dir})
+        fs.createReadStream(dir + '/' + fileName).pipe(extract)
+        extract.on('close', function () {
+            core.info(' 解压完成 ')
+
+            fs.unlinkSync(dir + '/' + fileName)
+        })
+        extract.on('error', function (err) {
+            core.error(err)
+        })
+    })();
 }
