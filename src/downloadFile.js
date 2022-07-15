b06df2da742967a3e6470e6a0c7af970912a03c2
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-15 11:02:04 +0800

    33

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -11,15 +11,15 @@ function downloadFile(url, fileName, dir){
     //     core.info('文件【' + fileName + '】下载完毕')
     // })
 
-    var express = require('express').Router()
-    // var router = express.Router()
+    var express = require('express')
+    var router = express.Router()
     var unzip = require('unzip')
     const download = require('download')
 
-    (async () => {  // err
+    // (async () => {  // err
         // core.info('kaishi')
-        // fs.writeFileSync('./123.zip', await download('https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
-        // ))
+        fs.writeFileSync('./123.zip', download('https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+        ))
         // core.info('结束')
         // var extract = unzip.Extract({ path: './'})
         // fs.createReadStream('./123.zip').pipe(extract)
@@ -30,5 +30,5 @@ function downloadFile(url, fileName, dir){
         // })
         // extract.on('error', function (err) {
         // })
-    })
+    // })
 }
