2702d1377aa69640ba2a5b58c9a93a7e2548f233
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-15 10:23:12 +0800

    1234

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -20,7 +20,7 @@ function downloadFile(url, fileName, dir){
         core.info('kaishi')
         fs.writeFileSync('./123.zip', await download('https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
         ))
-        core.info('结束')
+        // core.info('结束')
         // var extract = unzip.Extract({ path: './'})
         // fs.createReadStream('./123.zip').pipe(extract)
         // extract.on('close', function () {
