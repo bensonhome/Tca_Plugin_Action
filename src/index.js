37a4116750b8d003231d66ac5cabd9ea7e640f1c
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-15 15:34:06 +0800

    1234

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ if(os=='linux'){
     var cmd_init = 'codepuppy.exe quickinit --label ' + label
     var cmd_scan = 'codepuppy.exe quickscan --label ' + label + ' -s ' + process.cwd()
     var cwd = __dirname + '/../lib/tca-client-windows'
-    downloadFile.downloadFile(settings.windowsURL, settings.windowsName, settings.dir)
+    // downloadFile.downloadFile(settings.windowsURL, settings.windowsName, settings.dir)
 }else if(os=='darwin'){
     var cmd_init = './codepuppy quickinit --label ' + label
     var cmd_scan = './codepuppy quickscan --label ' + label + ' -s ' + process.cwd()
     var cwd = __dirname + '/../lib/tca-client-mac'
-    downloadFile.downloadFile(settings.darwinURL, settings.darwinName, settings.dir)
+    // downloadFile.downloadFile(settings.darwinURL, settings.darwinName, settings.dir)
 }
 
 const child_process = require('child_process')
@@ -48,7 +48,7 @@ try {  // client 启动
 
 try{  // 输出json分析报告
     const fs = require('fs')
-    fs.readFile(cwd + '/tca_quick_scan_report.json', (err, buffer) => {
+    fs.readFileSync(cwd + '/tca_quick_scan_report.json', (err, buffer) => {
     if(err) console.error(err)
     let d = buffer.toString()
     core.info(d)
