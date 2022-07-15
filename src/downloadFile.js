0980fe2e7f6244e9a1839a0748ec1a05f07a97a4
zthzhang111 <96770128+zthzhang111@users.noreply.github.com>
2022-07-15 10:15:38 +0800

    1

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -10,23 +10,23 @@ function downloadFile(url, fileName, dir){
     // request(url).pipe(stream).on('close', function(err){
     //     core.info('文件【' + fileName + '】下载完毕')
     // })
-    core.info('还没事.................................')
+
     var express = require('express')
     var router = express.Router()
     var unzip = require('unzip')
     const download = require('download')
-    core.info('报错了.................................')
+    core.info('运行到这里了。。。。。。。。。。。。。')
     (async () => {
-        core.info('运行进去了.................................')
-        // fs.writeFileSync('./tca-client-v20220629.1-x86_64-linux.zip', await download('https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
+        core.info('运行进来了。。。。。。。。。。。。。。。。')
+        // fs.writeFileSync('./123.zip', await download('https://github.com/Tencent/CodeAnalysis/releases/download/20220629.1/tca-client-v20220629.1-x86_64-linux.zip'
         // ))
-        // core.info('结束')
+        core.info('结束。。。。。')
         // var extract = unzip.Extract({ path: './'})
-        // fs.createReadStream('./tca-client-v20220629.1-x86_64-linux.zip').pipe(extract)
+        // fs.createReadStream('./123.zip').pipe(extract)
         // extract.on('close', function () {
         //     core.info(' 解压完成 ')
 
-        //     fs.unlinkSync('./tca-client-v20220629.1-x86_64-linux.zip')
+        //     fs.unlinkSync('./123.zip')
         // })
         // extract.on('error', function (err) {
         // })
